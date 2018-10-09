95eccda4c3d1d6e78d64f9283f3fe70d9fd33ce7
Dmitri <unabl4@gmail.com>
2018-10-09 15:34:57 +0300

    'Apple and Orange' challenge solution (Python 3)

---
+++ apple_and_orange/apple_and_orange.py
@@ -0,0 +1,40 @@
+# https://www.hackerrank.com/challenges/apple-and-orange/problem
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the countApplesAndOranges function below.
+def countApplesAndOranges(s, t, a, b, apples, oranges):
+    r = range(s,t+1)
+    print(sum(a+x in r for x in apples))
+    print(sum(b+x in r for x in oranges))
+
+if __name__ == '__main__':
+    st = input().split()
+
+    s = int(st[0])
+
+    t = int(st[1])
+
+    ab = input().split()
+
+    a = int(ab[0])
+
+    b = int(ab[1])
+
+    mn = input().split()
+
+    m = int(mn[0])
+
+    n = int(mn[1])
+
+    apples = list(map(int, input().rstrip().split()))
+
+    oranges = list(map(int, input().rstrip().split()))
+
+    countApplesAndOranges(s, t, a, b, apples, oranges)
