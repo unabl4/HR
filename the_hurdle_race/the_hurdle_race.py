8733e800d1b5cf72477715daed4a5e6c08fb5f99
Dmitri <unabl4@gmail.com>
2018-10-11 16:23:46 +0300

    'The Hurdle Race' challenge solution (Python 3)

---
+++ the_hurdle_race/the_hurdle_race.py
@@ -0,0 +1,40 @@
+# https://www.hackerrank.com/challenges/the-hurdle-race/problem
+
+#!/bin/python3
+
+# Complete the hurdleRace function below.
+def hurdleRace(k, h):
+    return max(0, max(h)-k)
+
+# ---
+
+n, k = map(int, input().split())
+height = list(map(int, input().rstrip().split()))
+result = hurdleRace(k, height)
+print(result)# https://www.hackerrank.com/challenges/the-hurdle-race/problem
+
+#!/bin/python3
+
+# Complete the hurdleRace function below.
+def hurdleRace(k, h):
+    return max(0, max(h)-k)
+
+# ---
+
+n, k = map(int, input().split())
+height = list(map(int, input().rstrip().split()))
+result = hurdleRace(k, height)
+print(result)# https://www.hackerrank.com/challenges/the-hurdle-race/problem
+
+#!/bin/python3
+
+# Complete the hurdleRace function below.
+def hurdleRace(k, h):
+    return max(0, max(h)-k)
+
+# ---
+
+n, k = map(int, input().split())
+height = list(map(int, input().rstrip().split()))
+result = hurdleRace(k, height)
+print(result)
