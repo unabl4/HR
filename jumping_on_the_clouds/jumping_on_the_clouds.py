9f48cbf99c541bebd9b5bab4bdc7ddf1bbf530f0
Dmitri <unabl4@gmail.com>
2018-10-26 19:55:07 +0300

    'Jumping on the Clouds' challenge solution (Python 3), rename the 'revisited' version of this challenge file name

--- jumping_on_the_clouds/jumping_on_the_clouds.py
+++ jumping_on_the_clouds/jumping_on_the_clouds.py
@@ -1,25 +1,28 @@
-# https://www.hackerrank.com/challenges/jumping-on-the-clouds-revisited/problem
+# https://www.hackerrank.com/challenges/jumping-on-the-clouds/problem
 
 #!/bin/python3
 
 # Complete the jumpingOnClouds function below.
-def jumpingOnClouds(c, k):
+def jumpingOnClouds(c):
     n = len(c)
-    e = 100
-    i = 0
-    t = 0
-    while not (i % n == 0 and t):
-        t = 1
-        e -= 1
-        if c[i]:
-            e -= 2
-        i += k
-        i %= n
+    def g(i,j):
+        # i - current index (cloud)
+        # j - number of jumps
+        
+        # recursion base case
+        if i == n-1: # end cloud?
+            return j # return the number of jumps so far
+        
+        r = [i+1,i+2] # range
+        return min(g(v,j+1) for v in r if v < n and not c[v])    
     
-    return e
+    return g(0,0)
     
-n,k = map(int, input().split())
+    
+# ---
+
+n = int(input())
 c = list(map(int, input().rstrip().split()))
 
-result = jumpingOnClouds(c, k)
-print(result)
+result = jumpingOnClouds(c)
+print(result) # min number of jumps
